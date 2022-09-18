808b112791c27a9d7426354af2a53701306b8128
Dinh Van Duc <dinhduc2550@gmail.com>
2022-09-18 18:30:53 +0700

    add

---
+++ basic-course-project/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients:Ingredient[] = [
+      new Ingredient("Thịt lợn",10),
+    new Ingredient("Apple",23),
+    new Ingredient("Tomatoes",10)
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
