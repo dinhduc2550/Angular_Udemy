f79002e25b0b05ff68d3407b33ddea088d340160
ADMIN\Admin <dinhduc2550@gmail.com>
2022-09-20 11:52:21 +0700

    update

--- conponents-databinding-deep-dive/src/app/cockpit/cockpit.component.ts
+++ conponents-databinding-deep-dive/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,23 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class CockpitComponent implements OnInit {
   newServerName = '';
   newServerContent = '';
+  @Output() serverCreated = new EventEmitter<{serverName:string,serverContent:string}>();
+  
+  @Output() bluePrintCreated = new EventEmitter<{serverName:string,serverContent:string}>();
   constructor() { }
 
   ngOnInit(): void {
   }
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent});
   }
 
   onAddBlueprint() {
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.bluePrintCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent});
+
   }
 }
