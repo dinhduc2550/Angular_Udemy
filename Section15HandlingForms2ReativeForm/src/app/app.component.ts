914c2a46b139cab95b522944add18f5e7527f91a
ADMIN\Admin <dinhduc2550@gmail.com>
2022-10-03 14:13:27 +0700

    update

--- Section15HandlingForms2ReativeForm/src/app/app.component.ts
+++ Section15HandlingForms2ReativeForm/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validator, Validators} from "@angular/forms";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,15 +16,38 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData2': new FormGroup({
         'userName': new FormControl(null, [Validators.required,this.forbiddenNames.bind(this)]),
-        'email2': new FormControl(null, [Validators.required, Validators.email]),
+        'email2': new FormControl(null, [Validators.required, Validators.email],this.forbiddenEmails),
       }),
       'gender2': new FormControl('male'),
       'hobbies2': new FormArray([])
     });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // )
+    this.signupForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    )
+    this.signupForm.setValue({
+      'userData2':{
+        'userName':'San',
+        'email2':'dinhduc2550@gmail.com'
+      },
+      'gender2':'male',
+      'hobbies2':[]
+    })
+    //patch value chi set value cho att duoc set khong set nhung thu con lai
+    this.signupForm.patchValue({
+      'userData2':{
+        'userName':'Anna',
+      }
+    })
   }
 
   onSubmit() {
     console.log(this.signupForm)
+
+    //reset specific att obbject
+    this.signupForm.get('userData2').get('userName').reset()
   }
 
   //add form
@@ -44,4 +68,19 @@ export class AppComponent implements OnInit {
     }
     return null
   }
+
+  forbiddenEmails(control: FormControl):Promise<any>|Observable<any>{
+    const promise = new Promise<any>((resolve,reject)=>{
+      setTimeout(()=>{
+        if(control.value === 'test@test.com'){
+          resolve({'emailIsForbidden':true})
+        }else{
+          resolve(null)
+        }
+      },1500)
+    })
+    return promise
+  }
 }
+
+
